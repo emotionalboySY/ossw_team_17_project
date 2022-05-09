e642ab4a7a6ab3977f77c08bc3fd5e0b0a81fcdb
kimgostring <kimgostring@naver.com>
2022-05-09 20:16:32 +0900

    Add Enum PlayMode, Add default const for dual mode

--- app/src/main/java/com/cauossw/snake/DefaultConst.java
+++ app/src/main/java/com/cauossw/snake/DefaultConst.java
@@ -2,10 +2,22 @@ package com.cauossw.snake;
 
 public class DefaultConst {
     static final int WIDTH = 40;
-    static final int HEIGHT = 40;
+    static final int HEIGHT = 80;
 
     static final int SNAKE_LENGTH = 4;
     static final int SNAKE_SPEED = 300;
-    static final Direction SNAKE_DIR = Direction.UP;
 
+    static final int SNAKE_DUAL_APPLE_NUM = 2;
+    static final int SNAKE_DUAL_1_X = 0;
+    static final int SNAKE_DUAL_1_Y = 0;
+    static final Direction SNAKE_DUAL_1_DIR = Direction.DOWN;
+
+    static final int SNAKE_DUAL_2_X = WIDTH - 1;
+    static final int SNAKE_DUAL_2_Y = HEIGHT - 1;
+    static final Direction SNAKE_DUAL_2_DIR = Direction.UP;
+
+    static final int SNAKE_SINGLE_APPLE_NUM = 2;
+    static final int SNAKE_SINGLE_X = WIDTH / 2;
+    static final int SNAKE_SINGLE_Y = HEIGHT / 2;
+    static final Direction SNAKE_SINGLE_DIR = Direction.UP;
 }
---
+++ app/src/main/java/com/cauossw/snake/PlayMode.java
@@ -0,0 +1,3 @@
+package com.cauossw.snake;
+
+public enum PlayMode { Single, Dual, Auto }
